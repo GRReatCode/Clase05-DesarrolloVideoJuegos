2408cd4bdfbdc0a8885e94997d9bfe9f1587aab1
Ruggeri Raúl Guillermo <ruggeriguillermo@gmail.com>
2022-07-25 01:40:42 -0300

    Commit  Scripts
    Se agregan scripts con funcionalidades personalizadas

---
+++ Desafio3/Assets/Scripts/Cannon.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public GameObject BalaPrefab;
+    public float TiempoEspera = 3f;
+    public float TiempoIntervalo = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Disparo", TiempoEspera, TiempoIntervalo);
+        Debug.Log("Disparo del cañon");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void Disparo()
+    {
+        Instantiate(BalaPrefab,transform);
+    }
+}
